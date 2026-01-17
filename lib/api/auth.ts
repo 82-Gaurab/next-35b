e4fb7ab33e73dbcd6b69d2e52b1694012cb5202b
GAURAB <gaurabkhadka4082@gmail.com>
2026-01-17 09:40:49 +0545

    fix axios import

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,19 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // Note: Actual backend API calls
-import axios from "axios"; // info: axios instance with base URL
+import axios from "./axios"; // info: axios instance with base URL
 import { API } from "./endpoint";
 
 export const register = async(registerData: any) => {
   try {
-    console.log("Auth bitra aayo")
-    console.log(registerData);
-    console.log(`Route : ${API.AUTH.REGISTER}`);
-    const response = await axios.post(`http://localhost:5050${API.AUTH.REGISTER}`, registerData);
-    console.log("response :")
-    console.log(response);
+    const response = await axios.post(API.AUTH.REGISTER, registerData);
     return response.data; // response ko body (what backend returns)
   } catch (error: Error | any) {
-    console.log("Auth ko error bitra aayo");
     // info: if 4xx/5xx error, axios throws error
     throw new Error(
       error.response?.data?.message // backend error message
